de90959bf265cbc3beb69895ab85eb5b7f0b6bd0
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-07-12 13:05:58 +0300

    Simple Black Jack upd.

--- Final_project/Black_Jack_game.py
+++ Final_project/Black_Jack_game.py
@@ -19,17 +19,75 @@ class Hand:
     def add_card(self, card):
         self.cards.append(card)
         self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
 
     def adjust_for_ace(self):
-        pass
+        if self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
 
 
 def main():
-    player = MixinPlayer(input("Enter your name player:"))
-    print(player.name)
-    player.skill()
+    #   Create deck
+    play_game = True
     deck = Deck()
-    print(deck)
+    player_hand = Hand()
+
+    deck.shuffle()
+
+    while play_game:
+        player_choice_1 = input("Do you want to play a game?:")
+        if player_choice_1 == "Yes":
+            player_hand.cards = []
+            player_hand.value = 0
+            player_hand.aces = 0
+
+            player_hand.add_card(deck.deal_one())
+            player_hand.add_card(deck.deal_one())
+            player_hand.adjust_for_ace()
+
+            print("Your cards:")
+            for card in player_hand.cards:
+                print(card)
+            print(f"You sum value:{player_hand.value}")
+
+            if player_hand.value == 21:
+                print("You Win")
+                break
+            player_choice_2 = input("Do you need a card? ")
+            while player_choice_2 != "Yes":
+                if player_choice_2 == "No":
+                    print("Chicken!")
+                    break
+                print("You drunk?")
+                player_choice_2 = input("Do you need a card? ")
+
+            while player_choice_2 == "Yes":
+                player_hand.add_card(deck.deal_one())
+                player_hand.adjust_for_ace()
+                print("Your cards: ")
+                for card in player_hand.cards:
+                    print(card)
+                print(f"You sum value:{player_hand.value}")
+                if player_hand.value > 21:
+                    print("You Loose")
+                    break
+                elif player_hand.value == 21:
+                    print("You Win")
+                    break
+                player_choice_2 = input("Do you need a card? ")
+                while player_choice_2 != "Yes":
+                    if player_choice_2 == "No":
+                        print("Chicken!")
+                        break
+                    print("You drunk?")
+                    player_choice_2 = input("Do you need a card? ")
+
+        elif player_choice_1 == "No":
+            play_game = False
+        else:
+            print("I don't understand you")
 
 
 if __name__ == "__main__":
