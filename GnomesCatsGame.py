04a94a0c1366ec72fec80d90f4dce9a5ac0c95a0
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-08-07 17:04:51 +0300

    created upd.

--- GnomesCatsGame.py
+++ GnomesCatsGame.py
@@ -1,4 +1,119 @@
 #!/usr/bin/env python3
-#Here are we start again!
+# Here are we start again!
+import pygame
 
-print("Hello")
+from pygame.locals import (
+
+    K_UP,
+
+    K_DOWN,
+
+    K_LEFT,
+
+    K_RIGHT,
+
+    K_ESCAPE,
+
+    KEYDOWN,
+
+    QUIT,
+
+)
+
+
+def shutdown_func(game=True):
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                game = False
+
+        elif event.type == pygame.QUIT:
+            game = False
+
+    return game
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, screenw, screenh):
+
+        super(Player, self).__init__()
+
+        self.surf = pygame.Surface((75, 25))
+
+        self.surf.fill((255, 255, 255))
+
+        self.rect = self.surf.get_rect()
+
+        self.screenw = screenw
+        self.screenh = screenh
+
+    def update(self, pressed_keys):
+
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        if self.rect.right > self.screenw:
+            self.rect.right = self.screenw
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= self.screenh:
+            self.rect.bottom = self.screenh
+
+
+def main():
+    pygame.init()
+    # Define constants for the screen width and height
+
+    SCREEN_WIDTH = 800
+
+    SCREEN_HEIGHT = 600
+
+    player_1 = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    game_on = True
+    while game_on:
+
+        game_on = shutdown_func()
+
+        # Get all the keys currently pressed
+
+        pressed_keys = pygame.key.get_pressed()
+
+        # Update the player sprite based on user keypresses
+
+        player_1.update(pressed_keys)
+
+        screen.fill((0, 0, 255))
+
+        pygame.draw.circle(screen, (255, 200, 255), (250, 250), 100)
+
+        surf = pygame.Surface((50, 50))
+
+        surf.fill((0, 0, 0))
+
+        screen.blit(player_1.surf, player_1.rect)
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -4,3 +4,25 @@ Test game to improve skills
 10.09.21
 
 1) [Create](https://www.youtube.com/watch?v=05nLdIVfSRU) my site on GitHub!
+2) https://realpython.com/pygame-a-primer/
+
+ Since this is a tutorial game, let’s design some basic gameplay for it as well:
+
+    The goal of the game is to avoid incoming obstacles:
+        The player starts on the left side of the screen.
+        The obstacles enter randomly from the right and move left in a straight line.
+    The player can move left, right, up, or down to avoid the obstacles.
+    The player cannot move off the screen.
+    The game ends either when the player is hit by an obstacle or when the user closes the window.
+
+When he was describing software projects, a former colleague of mine used to say, “You don’t know what you do until you know what you don’t do.” With that in mind, here are some things that won’t be covered in this tutorial:
+
+    No multiple lives
+    No scorekeeping
+    No player attack capabilities
+    No advancing levels
+    No boss characters
+
+You’re free to try your hand at adding these and other features to your own program.
+
+Let’s get started!
